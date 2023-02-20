920dccb80e5adb73dc356894bc94a59d4c58efb1
DLBY <david.lobry96@gmail.com>
2023-02-20 14:59:21 +0100

    WIP : How it works section

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -1,13 +1,21 @@
 import { Steps, Button } from "@/components";
 import { howItWorks } from '../data/howItWorks';
+import clsx from "clsx";
 
 export const HowItWorks = () => {
   return (
-    <section className="px-6 my-[200px]">
-      <h3 className="font-serif font-bold text-2xl text-center text-grey">
+    <section className="px-6 md:px-[39px] xl:px-[165px] my-[200px]">
+      <h3 className="font-serif font-bold text-2xl text-center text-grey md:text-left">
         How it works
       </h3>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-16">
+
+      <div className="relative hidden md:block md:mt-20 h-[2px] w-3/4 bg-paleOrange">
+        <div className="hidden md:block absolute h-[31px] w-[31px] bg-lightCream border-2 rounded-full border-darkCyan -top-[16px] left-0"></div>
+        <div className="hidden md:block absolute left-1/2 h-[31px] w-[31px] bg-lightCream border-2 rounded-full border-darkCyan -top-[16px]"></div>
+        <div className="hidden md:block absolute right-0 h-[31px] w-[31px] bg-lightCream border-2 rounded-full border-darkCyan -top-[16px]"></div>
+      </div>
+
+      <div className={clsx("grid grid-cols-1 gap-14 mt-16", "md:grid-cols-3 md:gap-5 xl:gap-24 relative")}>
         {howItWorks.map((step, index) => (
           <Steps
             key={index}
@@ -17,7 +25,7 @@ export const HowItWorks = () => {
           />
         ))}
       </div>
-      <div className="flex flex-col items-center mt-16">
+      <div className={clsx("flex flex-col items-center mt-16", "md:items-start md:mt-11 xl:mt-16")}>
         <Button link="/plan" text="Create your plan" className="text-lightCream bg-darkCyan hover:bg-cyan px-8 py-4 rounded-md transition-colors font-serif text-lg leading-[25px]" />
       </div>
     </section>
--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 interface StepsProps {
   title?: string;
   stepNumber: string;
@@ -6,23 +8,22 @@ interface StepsProps {
 }
 
 export const Steps = ({
-  title,
   stepNumber,
   stepTitle,
   stepDescription,
 }: StepsProps) => {
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex flex-col items-center mt-16">
+    <div>
+      <div className="flex flex-col items-center md:items-start ">
         <div className="flex items-center justify-center">
           <span className="text-paleOrange font-bold font-serif text-7xl">
             {stepNumber}
           </span>
         </div>
-        <h3 className="font-serif font-bold text-[28px] leading-[32px] text-darkGreyBlue text-center mt-6">
+        <h3 className={clsx("font-serif font-bold text-[28px] leading-[32px] text-darkGreyBlue text-center mt-6", "md:text-left md:w-2/3", "xl:text-[32px] xl:leading-[36px] xl:mt-[38px]")}>
           {stepTitle}
         </h3>
-        <p className="text-center text-[15px] leading-[25px] mt-6">
+        <p className={clsx("text-darkGreyBlue text-center text-[15px] leading-[25px] mt-6", "md:text-left xl:text-[16px] xl:leading-[26px] xl:mt-[42px]")}>
           {stepDescription}
         </p>
       </div>
